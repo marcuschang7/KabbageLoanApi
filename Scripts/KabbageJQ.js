d5ae6e7fbdb02ac76ba2a586dc4360b3ef7b18e6
MarcusChang7 <marcuschang@marcuschang.com>
2016-06-29 04:41:39 -0400

    2nd Update

--- Scripts/KabbageJQ.js
+++ Scripts/KabbageJQ.js
@@ -1,7 +1,22 @@
 ﻿$(document).ready(function () {
+
     var vd = $('#txtQualify').val();
-    if (vd == 'Congratulations your Qualified' || vd == 'Sorry your not Qualified' || vd == 'Sorry this service is currently down or invalid values were entered.') {
+    if (vd == 'Qualified' || vd == 'NotQualified' || vd == 'TryAgain') {
+        if (vd == 'Qualified') {
+            $('#Status').text("Your Pre-Approved Click the Apply Now button to continue");
+            $('#btnApply').show();
+        }
+        if (vd == 'NotQualified') {
+            $('#divBTNapplynow').hide();
+            $('#Status').text("Sorry your not approved");
+        }
+        if (vd == 'TryAgain') {
+            $('#divBTNapplynow').hide();
+            $('#Status').text("Please check your entries or try again later");
+        }
+
         $('#myModal').modal('show');
+
 
     }
 });
